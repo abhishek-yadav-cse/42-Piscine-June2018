21423de38a9efc9cac9c3d9387a3be99f1b21cb4
Abhishek Yadav <32717256+abhishek-yadav-cse@users.noreply.github.com>
2018-07-21 13:07:57 -0700

    Add files via upload

---
+++ Day11/ex07/ft_list_at.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_at.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadav <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/04 18:26:24 by ayadav            #+#    #+#             */
+/*   Updated: 2018/07/04 18:26:27 by ayadav           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (begin_list)
+	{
+		if (i == nbr)
+			return (begin_list);
+		begin_list = begin_list->next;
+		i++;
+	}
+	return (0);
+}
