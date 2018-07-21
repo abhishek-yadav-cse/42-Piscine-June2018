877e39c6c931ca15e79ce07c5784c4ba22624289
Abhishek Yadav <32717256+abhishek-yadav-cse@users.noreply.github.com>
2018-07-21 13:06:45 -0700

    Add files via upload

---
+++ Day11/ex02/ft_list_push_front.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_front.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadav <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/04 18:19:40 by ayadav            #+#    #+#             */
+/*   Updated: 2018/07/04 18:19:44 by ayadav           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list *elem;
+
+	elem = ft_create_elem(data);
+	elem->next = *begin_list;
+	*begin_list = elem;
+}
