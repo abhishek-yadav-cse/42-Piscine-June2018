be1753b886ee6251324befa79708070a7f95ea56
Abhishek Yadav <32717256+abhishek-yadav-cse@users.noreply.github.com>
2018-07-21 13:08:24 -0700

    Add files via upload

---
+++ Day11/ex09/ft_list_foreach.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadav <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/04 18:36:02 by ayadav            #+#    #+#             */
+/*   Updated: 2018/07/04 18:36:05 by ayadav           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	while (begin_list)
+	{
+		(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
