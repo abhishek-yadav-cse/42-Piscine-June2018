50004973aba763b736f0a0aa22a9a3e874f3eda0
Abhishek Yadav <32717256+abhishek-yadav-cse@users.noreply.github.com>
2018-07-21 13:08:09 -0700

    Add files via upload

---
+++ Day11/ex08/ft_list_reverse.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_reverse.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadav <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/04 18:35:27 by ayadav            #+#    #+#             */
+/*   Updated: 2018/07/04 18:35:30 by ayadav           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_reverse(t_list **begin_list)
+{
+	t_list	*prev;
+	t_list	*next;
+	t_list	*curr;
+
+	prev = 0;
+	next = 0;
+	curr = *begin_list;
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*begin_list = prev;
+}
