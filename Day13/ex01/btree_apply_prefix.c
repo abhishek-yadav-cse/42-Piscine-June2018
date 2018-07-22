cf68e4e9c4e998bd93364caafdacf8c16d1bca44
Abhishek Yadav <32717256+abhishek-yadav-cse@users.noreply.github.com>
2018-07-22 10:34:40 -0700

    Add files via upload

---
+++ Day13/ex01/btree_apply_prefix.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_prefix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadav <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/06 23:06:09 by ayadav            #+#    #+#             */
+/*   Updated: 2018/07/06 23:06:21 by ayadav           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_apply_prefix(t_btree *root, void (*applyf)(void*))
+{
+	if (root == 0)
+		return ;
+	applyf(root->item);
+	btree_apply_prefix(root->left, applyf);
+	btree_apply_prefix(root->right, applyf);
+}
---
+++ Day13/ex01/ft_btree.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadav <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/06 23:06:32 by ayadav            #+#    #+#             */
+/*   Updated: 2018/07/06 23:06:38 by ayadav           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+
+typedef struct	s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}				t_btree;
+
+void			btree_apply_prefix(t_btree *root, void (*applyf)(void*));
+#endif
