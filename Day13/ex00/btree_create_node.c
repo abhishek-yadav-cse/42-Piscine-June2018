9ea6676d84f9a452a3614fafcb8ad2c7318a9430
Abhishek Yadav <32717256+abhishek-yadav-cse@users.noreply.github.com>
2018-07-22 10:34:24 -0700

    Add files via upload

---
+++ Day13/ex00/btree_create_node.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_create_node.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadav <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/06 23:05:25 by ayadav            #+#    #+#             */
+/*   Updated: 2018/07/06 23:05:32 by ayadav           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+#include <stdlib.h>
+
+t_btree	*btree_create_node(void *item)
+{
+	t_btree	*node;
+
+	node = (t_btree*)malloc(sizeof(t_btree));
+	node->item = item;
+	node->left = 0;
+	node->right = 0;
+	return (node);
+}
