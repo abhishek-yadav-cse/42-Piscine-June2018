01837d5d5e6bd219e6ef80ee84d4295a399dc43c
Abhishek Yadav <32717256+abhishek-yadav-cse@users.noreply.github.com>
2018-07-22 10:35:36 -0700

    Add files via upload

---
+++ Day13/ex04/ft_btree.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayadav <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/06 23:08:41 by ayadav            #+#    #+#             */
+/*   Updated: 2018/07/06 23:08:45 by ayadav           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+
+typedef struct	s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}				t_btree;
+
+t_btree			*btree_create_node(void *item);
+void			btree_insert_data(t_btree **root, void *item,
+		int (*cmpf)(void*, void*));
+#endif
